0799fb79d32ccc3d679d052ca1a31a1ea760245c
hodu26 <gjdhks1212@gmail.com>
2026-04-01 16:58:21 +0900

    feat : STM 주기 스케줄링 설정 (SCRUM-43)
    - STM 드라이버를 사용하여 주기 스케줄러 설정
    - AppScheduling을 통해 1ms, 10ms ... 설정

--- Cpu0_Main.c
+++ Cpu0_Main.c
@@ -10,6 +10,40 @@
 // TODO: 센서 드라이버 합칠 때 아래 주석 해제
 // #include "SensorDriver.h"
 
+#include "Driver_Stm.h"
+
+/* CUSTOM MACRO */
+
+
+/***********************/
+
+/* CUSTOM GLOBAL VALUE */
+
+// App Scheduling
+typedef struct {
+    uint32 u32nuCnt1ms;
+    uint32 u32nuCnt10ms;
+    uint32 u32nuCnt100ms;
+    uint32 u32nuCnt1000ms;
+} StTestCnt;
+
+StTestCnt stTestCnt;
+
+/***********************/
+
+/* Function Prototypes */
+
+// App Scheduling
+void AppTask1ms(void);
+void AppTask10ms(void);
+void AppTask100ms(void);
+void AppTask1000ms(void);
+void AppScheduling(void);
+
+/****************************/
+
+/* Function Implementations */
+
 IFX_ALIGN(4) IfxCpu_syncEvent cpuSyncEvent = 0;
 
 void core0_main(void)
@@ -20,12 +54,21 @@ void core0_main(void)
     IfxCpu_emitEvent(&cpuSyncEvent);
     IfxCpu_waitEvent(&cpuSyncEvent, 1);
 
+    /* Init */
     Motor_Init();
     Door_Motor_Home();
     Window_Motor_Home();
 
+    Driver_Stm_Init();
+
+    /********/
+
+    Ifx_TickTime ticksFor1000ms = IfxStm_getTicksFromMilliseconds(&MODULE_STM0, 1000);
+
     while (1)
     {
+        AppScheduling();
+
         // ── 센서 폴링 ──────────────────────────────────────
         // TODO: 센서 드라이버 합칠 때 아래 블록 채워넣기
         //
@@ -56,3 +99,53 @@ void core0_main(void)
         Motor_Delay_ms(5);  // 루프 주기 5ms
     }
 }
+
+
+/******************/
+
+/* App Scheduling */
+
+void AppTask1ms(void)
+{
+    stTestCnt.u32nuCnt1ms++;
+}
+
+void AppTask10ms(void)
+{
+    stTestCnt.u32nuCnt10ms++;
+}
+
+
+void AppTask100ms(void)
+{
+    stTestCnt.u32nuCnt100ms++;
+}
+
+void AppTask1000ms(void)
+{
+    stTestCnt.u32nuCnt1000ms++;
+}
+
+void AppScheduling(void)
+{
+    if (stSchedulingInfo.u8nuScheduling1msFlag == 1u) {
+        stSchedulingInfo.u8nuScheduling1msFlag = 0u;
+        AppTask1ms();
+
+        if (stSchedulingInfo.u8nuScheduling10msFlag == 1u) {
+            stSchedulingInfo.u8nuScheduling10msFlag = 0u;
+            AppTask10ms();
+        }
+
+        if (stSchedulingInfo.u8nuScheduling100msFlag == 1u) {
+            stSchedulingInfo.u8nuScheduling100msFlag = 0u;
+            AppTask100ms();
+        }
+
+        if (stSchedulingInfo.u8nuScheduling1000msFlag == 1u) {
+            stSchedulingInfo.u8nuScheduling1000msFlag = 0u;
+            AppTask1000ms();
+        }
+    }
+}
+
